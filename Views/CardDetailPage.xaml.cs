a0774af6a69e7028bd1442447c7e0929cdb62614
Bagus Andryan Tresna <bagusandryan@outlook.com>
2023-12-09 22:53:14 +0100

    Implement AddNewCard and ViewModel in Consturctor

--- Views/CardDetailPage.xaml.cs
+++ Views/CardDetailPage.xaml.cs
@@ -1,14 +1,44 @@
-﻿namespace SWIFTcard.Views;
+﻿using CommunityToolkit.Maui.Core;
+using System.Threading;
+using SWIFTcard.Models;
+using static System.Net.Mime.MediaTypeNames;
+using CommunityToolkit.Maui.Alerts;
+using AndroidX.Lifecycle;
+using SWIFTcard.ViewModels;
+
+namespace SWIFTcard.Views;
 
 public partial class CardDetailPage : ContentPage
 {
-	public CardDetailPage()
-	{
-		InitializeComponent();
+    private CardDetailViewModel _viewModel;
+
+    public CardDetailPage(CardDetailViewModel viewModel)
+    {
+        InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
     }
 
-    void OnSwipedDown(System.Object sender, Microsoft.Maui.Controls.SwipedEventArgs e)
+    async void OnSwipedDown(System.Object sender, Microsoft.Maui.Controls.SwipedEventArgs e)
     {
-        Navigation.PopModalAsync();
+        await Navigation.PopModalAsync();
+    }
+
+    async void AddNewCard(System.Object sender, System.EventArgs e)
+    {
+        Card newCard = new Card();
+        newCard.Question = Question.EntryText;
+        newCard.Answer = Answer.EntryText;
+        newCard.Context = Context.EntryText;
+        newCard.Deck = _viewModel.GetActiveDeck();
+
+        await Navigation.PopModalAsync();
+
+        Question.HideKeyboard();
+        Answer.HideKeyboard();
+        Context.HideKeyboard();
+
+        var snackbar = Snackbar.Make("Card added");
+        await snackbar.Show();
     }
 }
